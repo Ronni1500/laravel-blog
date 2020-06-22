5cf321bebb04547d11aa97ce76f4e40f3ae1f54b
Roman Tsarkov <romatsat@gmail.com>
2020-06-22 22:50:16 +0300

    test commit

--- resources/views/layouts/particles/header.blade.php
+++ resources/views/layouts/particles/header.blade.php
@@ -1,7 +1,7 @@
 @section('sidebar')
-{{--    @foreach($menuitems as $item)--}}
-{{--        {{$item->title}}--}}
-{{--    @endforeach--}}
+    @foreach($menuitems as $item)
+        {{$item->title}}
+    @endforeach
     <header class="blog-header py-3">
         <div class="row flex-nowrap justify-content-between align-items-center">
             <div class="col-4 pt-1">
